910b5a9ea4a84ec83ab9577012aeda612e704f7a
suryaraj93 <68339683+suryaraj93@users.noreply.github.com>
2023-03-09 09:41:50 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser')
 const productRouter = require('./routes/productRoute')
 const reviewRouter = require('./routes/reviewRoute')
 const userRouter = require('./routes/userRoute')
+const cartRouter = require('./routes/cartRoute')
+const orderRouter = require('./routes/orderRoute')
 
 //env variables
 const port = process.env.PORT
@@ -17,6 +19,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/products', productRouter)
 app.use('/api/review', reviewRouter)
 app.use('/api/user',userRouter)
+app.use('/api/cart',cartRouter)
+app.use('/api/orders',orderRouter)
 
 //route
 app.get('/', (req, res) => {
